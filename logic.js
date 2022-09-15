2b66d07ed8f83f12a79250c1e39e84210a8aa354
Kimpes <gustafssonfilip@hotmail.com>
2022-09-15 22:06:32 +0200

    Still haven't gotten css to work

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 // const http = require('http');
-const fs = require('fs');
+// const fs = require('fs');
 
 // const myServer = http.createServer(function(req, res){
 //     res.statusCode = 200;
@@ -7,9 +7,44 @@ const fs = require('fs');
 //     res.end(<p>this is a response</p>);
 // });
 
-const express = require('express');
+const dummyData = require("./dummy-data");
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const app = express();
 
-myServer.listen(8080); //change to 80 later (supposedly the standard)
+//code cited from https://stackoverflow.com/questions/35133573/import-external-file-content-into-handlebar
+app.use(express.static("/public"));
+
+app.engine(
+  "hbs",
+  expressHandlebars.engine({
+    defaultLayout: "./main.hbs",
+  })
+);
+app.set("view engine", "hbs");
+
+// app.get("/", function (req, res) {
+//   const model = {
+//     humans: dummyData.humans,
+//   };
+//   res.render("show-all-humans.hbs", model);
+// });
+
+app.get("/works", function (req, res) {
+  res.render("works.hbs");
+});
+app.get("/index", function (req, res) {
+  res.render("index.hbs");
+});
+app.get("/blog", function (req, res) {
+  res.render("blog.hbs");
+});
+app.get("/qna", function (req, res) {
+  res.render("qna.hbs");
+});
+
+app.listen(8080);
+//change to 80 later (supposedly the standard)
 
 //this entire file should be modified to accomodate express intead of http
 // app.get ('/mainpage', function (req, res){}) is a middlewear
